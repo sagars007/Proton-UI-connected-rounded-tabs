979b156207b7cac766d365aad953c557f2d7857e
Sagar S <56472130+sagars007@users.noreply.github.com>
2021-06-07 21:21:49 +0400

    Add files via upload

---
+++ user.js
@@ -0,0 +1,24 @@
+//enable browser to read userChrome.css
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+//Full screen fade faster:
+user_pref("full-screen-api.transition-duration.enter", 50 50);
+user_pref("full-screen-api.transition-duration.leave", 50 50);
+
+//backspace button to go to previous page:
+user_pref("browser.backspace_action", 0);
+
+//compact mode option visible
+user_pref("browser.compactmode.show", true);
+
+//pocket disabled
+user_pref("extensions.pocket.enabled", false);
+
+//ctrltab cycles through recently used tabs first
+user_pref("browser.ctrlTab.sortByRecentlyUsed", true);
+
+//remove about:config open warning
+user_pref("browser.aboutConfig.showWarning", false);
+
+//security padlock icon switch to green color icon
+user_pref("security.secure_connection_icon_color_gray", false);
